cf9477dbff2cebc0b5e7a9bf88e6a923fdd8a15b
jenscobie <jenscobie@gmail.com>
2015-05-18 21:48:57 -0700

    My apps

--- modules/people/manifests/jenscobie.pp
+++ modules/people/manifests/jenscobie.pp
@@ -1,7 +1,22 @@
 class people::jenscobie {
 
   include dropbox
+  include evernote
   include onepassword
+  include scansnap
+  include skype
+
+  include osx::global::tap_to_click
+  include osx::dock::autohide
+  include osx::dock::clear_dock
+
+  include osx::software_update
+
+  include osx::global::natural_mouse_scrolling
+
+  class { 'osx::dock::icon_size':
+    size => 36
+  }
 
   class { 'intellij':
     edition => 'ultimate',
