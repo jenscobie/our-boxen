189952644279e1fa454d3df3a10a01c00e911c18
jenscobie <jenscobie@gmail.com>
2015-05-18 21:18:23 -0700

    Set versions

--- manifests/site.pp
+++ manifests/site.pp
@@ -71,12 +71,20 @@ node default {
   nodejs::version { 'v0.8': }
   nodejs::version { 'v0.10': }
 
+  class { 'nodejs::global':
+    version => 'v0.10'
+  }
+
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
+
+  class { 'ruby::global':
+    version => '2.1.2'
+  }
 
   # common, useful packages
   package {
