167f33d3aca53808507b9683c41044de955cdfac
jenscobie <jenscobie@gmail.com>
2015-09-24 12:20:31 -0700

    Install all the rubies and bundler

--- modules/myorg/manifests/init.pp
+++ modules/myorg/manifests/init.pp
@@ -7,5 +7,17 @@ class myorg::environment {
   include viscosity
   include wget
 
+  ruby::version { '1.9.3': }
+  ruby::version { '2.0.0': }
+  ruby::version { '2.1.0': }
+  ruby::version { '2.1.1': }
+  ruby::version { '2.1.2': }
+
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => '~> 1.10',
+    ruby_version => '*',
+  }
+
   include ruby::global
 }
