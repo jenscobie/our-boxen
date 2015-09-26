787106abd394723784d0b81c7fdf74db5ca57ec2
jenscobie <jenscobie@gmail.com>
2015-09-26 10:16:27 -0700

    Clone puppet-docker module by default

--- modules/projects/manifests/boxen.pp
+++ modules/projects/manifests/boxen.pp
@@ -8,4 +8,10 @@ class projects::boxen {
     source	=> 'jenscobie/puppet-vault',
     dir		  => "/Users/${::boxen_user}/Code/jenscobie/puppet-vault",
   }
+
+  boxen::project { 'puppet_docker':
+    source	=> 'jenscobie/puppet-docker',
+    dir		  => "/Users/${::boxen_user}/Code/jenscobie/puppet-docker",
+  }
+
 }
